acfa12fe461a376eaa937624057b015023599138
Muskaan <hasratnabiha@gmail.com>
2026-04-28 22:24:24 +0530

    frontend integration done

--- day-7-fullStack/backEnd/server.js
+++ day-7-fullStack/backEnd/server.js
@@ -12,5 +12,5 @@ const connectToDb = require("./src/config/database");
 connectToDb();
 
 app.listen(3000, ()=>{
-    console.log("server is running on port 3000");
+    console.log("server is running on port number 3000");
 })
--- day-7-fullStack/frontEnd/src/App.jsx
+++ day-7-fullStack/frontEnd/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import axios from "axios"
 
 function App() {
@@ -21,19 +21,59 @@ function App() {
     }
   ])
 
-  axios.get("http://localhost:3000/api/notes")
-.then((res)=>{
-  setNotes(res.data.notes)
-})
+  console.log("Hello Integration")
+
+  function fetchNotes(){
+    axios.get("http://localhost:3000/api/notes")
+      .then((res)=>{
+        setNotes(res.data.notes)
+      })
+  }
+
+  useEffect(()=>{
+      fetchNotes();
+  }, {})
+
+  function handleSubmit(e){
+    e.preventDefault();
+    const {title,description} = e.target.elements;
+    console.log(title.value , description.value);
+
+    axios.post("http://localhost:3000/api/notes", {
+      title : title.value ,
+      description : description.value
+    })
+    .then(res=>{
+      console.log(res.data)
+
+      fetchNotes();
+    })
+  }
+
+  function handleDeleteNote(noteId){
+    axios.delete("http://localhost:3000/api/notes/"+noteId)
+    .then(res=>{
+       console.log(res.data);
+       fetchNotes();
+    })
+  }
 
   return (
     <> 
+
+    <form className='note-create-form' onSubmit={handleSubmit}>
+       <input type="text" name='title' placeholder='Enter Title : ' />
+       <input type="text" name='description' placeholder='Enter Description : ' />
+       <button>Create Note</button>
+    </form>
+
      <div className="notes">
       {
          notes.map(note =>{
            return <div className="note">
          <h1>{note.title}</h1>
          <p>{note.description}</p>
+         <button onClick={()=>{handleDeleteNote(note._id)}}>Delete</button>
        </div>
          })
       }

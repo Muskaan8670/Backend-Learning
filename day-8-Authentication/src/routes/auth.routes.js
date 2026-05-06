e278b4a368197396aeda77552b6bb987196788ed
Muskaan <hasratnabiha@gmail.com>
2026-05-07 00:06:17 +0530

    day-8 of  backend learning

---
+++ day-8-Authentication/src/routes/auth.routes.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const authRouter = express.Router();
+const userModel = require("../models/user.model");
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+
+module.exports = authRouter;
+
+authRouter.post("/register", async (req,res)=>{
+    const {name , email , password} = req.body;
+
+    const isUserExists = await userModel.findOne({email});
+
+    if(isUserExists){
+        return res.status(409).json({
+            message : "User already exists"
+        })
+    }
+
+    const user = await userModel.create({
+        name,
+        email,
+        password : crypto.createHash("sha256").update(password).digest('hex') 
+    } )
+
+    const token = jwt.sign(
+        {
+          id : user._id,
+        },
+        process.env.JWT_SECRET,{expiresIn:"1hr"})
+
+    res.cookie("token",token )
+
+    res.status(201).json({
+        message : "User registered successfully",
+        user : {
+            name : user.name,
+            email : user.email
+        }
+    })
+})
+
+authRouter.get("/get-me", async (req,res)=>{
+        const token = req.cookies.token;
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        const user = await userModel.findById(decoded.id);
+
+        res.json({
+            name: user.name,
+            email: user.email,
+        });
+})
